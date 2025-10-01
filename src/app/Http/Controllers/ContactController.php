d869dfd15ce1534d706aca5530dfe881deaffb45
masa376 <geneara46@gmail.com>
2025-10-01 12:22:52 +0900

    Implement contact form (routes, controller, views, migration) + docker-compose adj.

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function confirm(Request $request)
+    {
+        $contact = $request->only(['name', 'email', 'tell', 'contact']);
+        Contact::create($contact);
+        return view('thanks');
+        return view('confirm', compact('contact'));
+    }
+
+    public function store()
+    {
+        $contact = $request->only(['name', 'email', 'tell', 'content']);
+    }
+}
